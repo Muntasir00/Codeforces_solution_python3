a26c2cacf987b40f7c14c5de7adaa1218b374fd4
Muntasir00 <muntasir690000@gmail.com>
2021-12-11 04:18:30 +0600

    749A solved

--- Untitled-1.py
+++ Untitled-1.py
@@ -1,18 +1,12 @@
-from collections import Counter, OrderedDict
-c = Counter()                           # a new, empty counter
-c = Counter('gallahad')
-c2 = Counter('agllaahdl')
-print(c==c2)
-#print(c2)
+print(int(4.2))
 
+# dictionary
+# class_list = dict() 
+# data = input('Enter name & score separated by ":" ') 
+# temp = data.split(':') class_list[temp[0]] = int(temp[1])  
 
-dictionary
-class_list = dict() 
-data = input('Enter name & score separated by ":" ') 
-temp = data.split(':') class_list[temp[0]] = int(temp[1])  
+# OR
 
-OR
-
-key = input("Enter key") 
-value = input("Enter value") 
-class_list[key] = [value] 
+# key = input("Enter key") 
+# value = input("Enter value") 
+# class_list[key] = [value] 
